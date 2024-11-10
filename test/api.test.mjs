d86f58c0c89e0e17e7e82635c118baa15951d68e
bilonda-b <bilondabelieve283@gmail.com>
2024-11-10 06:59:42 +0200

    new changes

--- test/api.test.mjs
+++ test/api.test.mjs
@@ -1,20 +1,55 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const expect = chai.expect; 
-chai.use(chaiHttp); 
+import * as chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../server.js';
 
-describe("GET /user", () => {
-    it('should return user data', (done) => {
-        chai.request('http://localhost:3000/user')
-        .get('/user/1')
-        .end((err, res) => {
-            expect(res).to.have.status(200);
-            expect(res.body).to.be.an('object');
-            expect(res.body).to.have.property('id', 1);
-            done();
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe("User API", () => {
+
+
+    describe("POST /Signup", () => {
+        it("should create a new user", (done) => {
+            const newUser = { email: "stephaniekalongo@gmail.com", password: "kalongo6985" };
+            chai.request(server)
+                .post('/Signup')
+                .send(newUser)
+                .end((err, res) => {
+                    expect(res).to.have.status(201);
+                    expect(res.body).to.be.an('object');
+                    expect(res.body).to.have.property('message', 'User created successfully');
+                    done();
+                });
+        });
+    });
+
+    
+    describe("GET /users", () => {
+        it("should retrieve all users", (done) => {
+            chai.request(server)
+                .get('/users')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('array');
+                    done();
+                });
+        });
+    });
+
+    
+    describe("POST /Signin", () => {
+        it("should login the user with valid credentials", (done) => {
+            const credentials = { username: "ysvanetti0@gmpg.org", password: "p4n`nFj" };
+            chai.request(server)
+                .post('/Signin')
+                .send(credentials)
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('object');
+                    expect(res.body).to.have.property('message', 'Login successful');
+                    done();
+                });
         });
     });
 });
-
-
 
